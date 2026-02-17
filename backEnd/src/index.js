3a983f0f748826a2fd5f07ca2339bcfe1764f9ae
abhishek goud <goud87041@gmail.com>
2026-02-17 21:47:58 +0530

    add model and setup express

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,20 +1,60 @@
+// require("dotenv").config()
+
+// const express = require("express")
+
+// const mongoose = require("mongoose")
+
+
+// const app = express()
+
+// app.use(express.json())
+
+// const port = process.env.PORT || 4000
+
+// (async ()=>{
+// try {
+//     await mongoose.connect(process.env.DB_URL)
+//     .then(()=>{
+//         app.listen(port , ()=>{
+//             console.log("server running at ", port)
+//         })
+//     })
+// } catch (error) {
+    
+// }})()
+
+
 require("dotenv").config()
-
-const express = require("express")
-
 const mongoose = require("mongoose")
-
+const express = require("express")
 
 const app = express()
 
+// console.log("app : ",app);
+
+
 app.use(express.json())
 
-const port = process.env.PORT || 4000
+const  PORT = process.env.PORT || 4000
 
-(async ()=>{
+console.log("PORT :",PORT);
+console.log("DB url : ",process.env.DB_URL);
+
+
+
+;(async()=>{
 try {
-    await mongoose.connect(process.env)
+    await mongoose.connect(process.env.DB_URL)
+    .then(()=>{
+        app.listen(PORT ,()=>{
+        console.log("server running at ",PORT);
+    }
+    )
+    })
 } catch (error) {
-    
-}})()
+    console.error("ERROR : ", error)
+}
+})()
+
+
 
---
+++ backEnd/src/models/user.model.js
@@ -0,0 +1,31 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema =new  mongoose.Schema({
+    name : {
+        type : String ,
+        required : true,
+    },
+    lastName : {
+        type : String , 
+        required : true
+    },
+    password : {
+        type : String,
+        required : [true,"password is require"]
+    },
+    email :{
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true ,
+        trim : true
+    },
+    userName : {
+        type : String ,
+        required : true,
+        unique : true
+    }
+
+},{timestamps:true})
+
+export const User = mongoose.model("User", userSchema)
